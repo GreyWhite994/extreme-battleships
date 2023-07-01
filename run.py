29246802027cdab91222b4d6f8c873e4f52d6a5e
Graham White <greywhite994@gmail.com>
2023-07-01 22:42:56 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+def create_board(size):
+    game_board = [[" "] * size for i in range(size)]
