bd10bf7e48e8578878ba564dccde61e0605f4bff
Graham White <greywhite994@gmail.com>
2023-07-01 23:14:57 +0000

    Created functions to create and print boards

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from random import randint
 
 def create_board(size):
-    game_board = [[" "] * size for i in range(size)]
+    board = [["."] * size for i in range(size)]
+    return board
+
+def print_board(board):
+    row_number = 1
+    for row in board:
+        print("|%s|" % ("|".join(row)))
+        row_number += 1
