6fe26a9910c02fbf2056a9d92cb7f1dddebab8f7
Graham White <greywhite994@gmail.com>
2023-07-06 12:38:01 +0000

    Created function to increment player/computer scores

--- run.py
+++ run.py
@@ -24,12 +24,23 @@ player_board = create_board(6)
 create_ships(3,"player", 6,player_board)
 print_board(player_board, "Gray")
 
-def make_guess(board):
+def increment_score(type):
+    player_score = 0
+    computer_score = 0
+    if type == 'Player':
+        player_score += 1
+    else:
+        computer_score +=1
+    print(player_score)
+
+def make_guess(board, type):
     x = int(input("Please enter a row: \n"))
     y = int(input("Please enter a column: \n"))
     if board[x-1][y-1] == '@':
         print("Hit")
+        increment_score(type)
     else:
         print("Miss")
 
-make_guess(player_board)
+make_guess(player_board, 'Player')
+
