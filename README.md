432b8c9a0a3a6628475ec991719ecfcd7ea6a00f
Graham White <greywhite994@gmail.com>
2023-07-13 21:01:44 +0000

    README.md introduction added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Extreme Battleships
+
+Extreme Battleships is a Python terminal game, which uses the Code Institute mock terminal on Heroku.
+
+Users can attempt to beat the computer by finding all their battleships before the computer can locate theirs. The user can set the board size and amount of ships that each player has thereby increasing or decreasing the difficulty. The game is over when either the computer or the player finds all the opposing ships. A draw is also possible if the final ship of both players is found on the same turn.
+
+The live version of the project can be found here - https://extreme-battleships-0e054539df86.herokuapp.com/
--- run.py
+++ run.py
@@ -4,7 +4,8 @@ scores = {"computer":0, "player":0}
 
 
 class Board:
-    """Main Board class. Sets up the board and has methods to create ships, print the board and validate guesses."""
+    """Main Board class. Sets up the board and has methods to create ships, print the board and validate guesses.
+    Inspiration for this code taken from 'Portfolio project 3 scope' https://youtu.be/4sqtzZQpDJE"""
     def __init__(self, size, name, ship_num, type):
         self.size=size
         self.name=name
@@ -159,7 +160,7 @@ def new_game():
     print("-" * 35)
     while True:
         try:
-            size = int(input("How big would you like the game board (e.g if 6 is entered the board will be a 6x6 grid), minimum is 3 and the limit is 10: \n"))
+            size = int(input("How big would you like the game board\n(e.g if 6 is entered the board will be a 6x6 grid), minimum is 3 and the limit is 10: \n"))
         except ValueError:
             print("Please enter a number")
             continue
@@ -170,7 +171,7 @@ def new_game():
     print("-" * 35)
     while True:
         try: 
-            ship_num = int(input("How many ships would you like each board to have(Minimum is 3 and the limit is 8): \n"))
+            ship_num = int(input("How many ships would you like each board to have\n(Minimum is 3 and the limit is 8): \n"))
         except ValueError:
             print("Please enter a number")
             continue 
