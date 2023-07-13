21e3c6255ebcd181d94b5df4b24bc35b32b6d482
Graham White <greywhite994@gmail.com>
2023-07-13 19:30:23 +0000

    Amalgamated create_board,create_ships,print_board under Board class

--- run.py
+++ run.py
@@ -2,107 +2,98 @@ from random import randint
 
 scores = {"computer":0, "player":0}
 
-def create_board(size):
-    """Creates a board by using the size parameter. Returns board"""
-    board = [["."] * size for i in range(size)]
-    return board
 
-def create_ships(ship_num, size, board):
-    """Creates ships by taking ship_num, size and board parameters. Prevents duplicate ship locations
-    by comparing to locations in ship_locations list. If ship_x, ship_y not in ship_locations a ship will be created and 
-    the location used will be appended to ship_locations"""
-    ship_locations = []
-    for location in range(ship_num):
+class Board:
+    def __init__(self, size, name, ship_num):
+        self.size=size
+        self.name=name
+        self.board=[["."] * size for i in range(size)]
+        self.ship_num=ship_num
+        self.guesses = []
+        self.ship_locations = []
+    
+    def create_ships(self):
+        for location in range(self.ship_num):
             while True:
-                ship_x,ship_y = randint(0, size - 1), randint(0, size - 1)
-                if ((ship_x,ship_y)) not in ship_locations:
-                    board[ship_x][ship_y] = '@'
-                    ship_locations.append((ship_x,ship_y))
+                ship_x,ship_y = randint(0, self.size - 1), randint(0, self.size - 1)
+                if ((ship_x,ship_y)) not in self.ship_locations:
+                    self.board[ship_x][ship_y] = '@'
+                    self.ship_locations.append((ship_x,ship_y))
                     break
 
-def print_board(board, name):
-    """Prints board by taking board and name parameters"""
-    print(f"{name}'s Board")
-    row_number = 1
-    for row in board:
-        print("|%s|" % ("|".join(row)))
-        row_number += 1
+    def print_board(self):
+        print(f"{self.name}'s Board")
+        row_number = 1
+        for row in self.board:
+            print("|%s|" % ("|".join(row)))
+            row_number += 1 
+
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "X"
+
+        if (x,y) in self.ship_locations:
+            self.board[x][y] = '*'
+            return 'Hit'
+        else:
+            return 'Miss'
 
 
-def player_guess(computer_board, computer_guess_board, name, size):
-    """Takes input from user in the form of x and y variables to guess computer ship location.
-    Validates x and y so as to disallow non-integers and guesses which are off grid or repeat guesses.
-    If x/y corresponds to a ship location player score is incremented and an * is printed in that location.
-    Else it is a miss and an X is printed to the location"""
+def player_guess(computer_board, name, size):
     print(f"{name}'s turn")
     while True:
-        while True:
-            try:
-                x = int(input("Please enter a row: \n"))
-            except ValueError:
-                print("Please enter a number!")
-                continue
-            if x >= 1 and x <= size:
-                break
-            else:
-                print(f"You must enter a number between 1 and {size}")
-        while True:
-            try:
-                y = int(input("Please enter a column: \n"))
-            except ValueError:
-                print("Please enter a number!")
-                continue
-            if y >= 1 and y <= size:
-                break
-            else:
-                print(f"You must enter a number between 1 and {size}")
-        if computer_board[x-1][y-1] =='X' or computer_board[x-1][y-1] =='*':
-            print("You already guessed that location!")
-        else:
+        try:
+            x = int(input("Please enter a row: \n"))
+        except ValueError:
+            print("Please enter a number!")
+            continue
+        if x >= 1 and x <= size:
             break
+        else:
+            print(f"You must enter a number between 1 and {size}")
+    while True:
+        try:
+            y = int(input("Please enter a column: \n"))
+        except ValueError:
+            print("Please enter a number!")
+            continue
+        if y >= 1 and y <= size:
+            break
+        else:
+            print(f"You must enter a number between 1 and {size}")
     print(f"{name} guessed {x},{y}")
-    if computer_guess_board[x-1][y-1] == '@':
+    result = Board.guess(computer_board, x-1, y-1)
+    if result == 'Hit':
         print("Hit")
-        computer_board[x-1][y-1] = '*'
         scores["player"] += 1
     else:
         print("Miss")
-        computer_board[x-1][y-1] = 'X'
+
 
 def computer_guess(player_board, size):
-    """Assigns a random integer to x,y varibales between 0 and the size of the grid - 1 to ensure
-    the guess will not go off grid. Validates x,y by not allowing guess if location corresponds to */X.
-    If a ship at x,y location computer score will be incremented and * will replace @. Else, X will replace @."""
     print("Computer's turn")
     x = randint(0, size - 1)
     y = randint(0, size - 1)
-    while True:
-        if player_board[x][y] =='X' or player_board[x][y] =='*':
-            x = randint(0, size - 1)
-            y = randint(0, size - 1)
-        else:
-            break
     print(f"Computer guessed {x+1},{y+1}")
-    if player_board[x][y] == '@':
+    result = Board.guess(player_board, x, y)
+    if result == 'Hit':
         print("Hit")
-        player_board[x][y] = '*'
         scores["computer"] += 1
     else:
         print("Miss")
-        player_board[x][y] = 'X'
 
-def play_game(player_board, computer_guess_board, computer_board, name, size, ship_num):
+def play_game(player_board, computer_board, name, size, ship_num):
     game_over = False
     while game_over != True:
-        player_guess(computer_board, computer_guess_board, name, size)
+        player_guess(computer_board, name, size)
         print("-" * 35)
         computer_guess(player_board, size)
         print("-" * 35)
         print(f"After this round the scores are {name}:{scores['player']} and Computer:{scores['computer']}")
         print("-" * 35)
-        print_board(computer_board, 'Computer')
+        Board.print_board(computer_board)
         print("-" * 35)
-        print_board(player_board, name)
+        Board.print_board(player_board)
         print("-" * 35)
         if scores["player"] == ship_num:
             print(f"{name} wins!")
@@ -154,15 +145,15 @@ def new_game():
             print("Number must be between 3 and 8!")
     print("-" * 35)
 
-    player_board = create_board(size)
-    computer_board = create_board(size)
-    computer_guess_board = create_board(size)
-    create_ships(ship_num, size, player_board)
-    create_ships(ship_num, size, computer_guess_board)
-    print_board(computer_board, 'Computer')
+    player_board = Board(size,name,ship_num)
+    computer_board = Board(size,"Computer",ship_num)
+    computer_guess_board = Board(size,"Computer",ship_num)
+    Board.create_ships(player_board)
+    Board.create_ships(computer_board)
+    Board.print_board(computer_board)
     print("-" * 35)
-    print_board(player_board, name)
+    Board.print_board(player_board)
     print("-" * 35)
-    play_game(player_board, computer_guess_board, computer_board, name, size, ship_num)
+    play_game(player_board, computer_board, name, size, ship_num)
 
 new_game()
